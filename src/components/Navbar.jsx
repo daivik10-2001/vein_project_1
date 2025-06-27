230c055c5d4b4b431c1cb1e079c4a723303d5900
daivik_m <daivikm41@gmail.com>
2025-06-27 22:41:29 +0530

    menu_responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import logo from '../assets/vlogo.png';
 import'../components/Navbar.css'
 import {  NavLink  } from 'react-router-dom';
+import menuicon from '../assets/more.png'
 const Navbar = () => {
     const [sticky, setSticky] = useState(false);
 
@@ -10,18 +11,23 @@ const Navbar = () => {
         window.scrollY > 10 ? setSticky(true) : setSticky(false);
       })
     },[]);
+      const[mobilemenu, setMobileMenu] = useState(false);
+    const toggleMenu = () => {
+      mobilemenu ? setMobileMenu(false) : setMobileMenu(true);
+    }
 
   return (
     <nav className={`container ${sticky ? 'dark_nav' : ''}`}>
     
       <img src={logo} alt="" className='logo'  />
-      <ul>
+      <ul className={mobilemenu ? '' : 'hide-mobile-menu'}>
      <NavLink to='/'><li>Home</li></NavLink>   
         <NavLink to='/about'><li>About</li></NavLink> 
         <NavLink to='/products'><li>Products</li></NavLink> 
         <NavLink to='/contact'><li><button className='btn'>Contact</button></li></NavLink> 
         
       </ul>
+      <img src={menuicon} alt="" className= 'menu-icon' onClick={toggleMenu} />
       
     </nav>
   )
